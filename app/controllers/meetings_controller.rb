b2b1cfc71bad92125f571ccafbf1a68af16f5155
Dana Scheider <dana.scheider@gmail.com>
2015-10-28 20:11:48 -0700

    Require the factory files this time

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,6 @@
 class MeetingsController < ApplicationController
   before_action :set_meeting, only: [:show, :edit, :update, :destroy]
-  before_filter :authorize, only: [:new]
+  before_filter :authorize, only: [:new, :edit, :create]
 
   # GET /meetings/1
   # GET /meetings/1.json
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl'
+
+Dir.glob("./spec/factories/**/*.rb") { |file| require file }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
